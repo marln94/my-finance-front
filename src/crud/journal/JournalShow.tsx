4a5ae1f86247c85996b3cc6aa65cb19ecfd91592
Marlon Calderón <mrln9eca@gmail.com>
2026-02-25 13:16:25 -0600

    feat: add toggle action

--- src/crud/journal/JournalShow.tsx
+++ src/crud/journal/JournalShow.tsx
@@ -1,16 +1,62 @@
 import {
   ArrayField,
   DateField,
+  EditButton,
   ReferenceField,
   Show,
   SimpleList,
   SimpleShowLayout,
   TextField,
+  TopToolbar,
+  Button,
+  useRecordContext,
+  useNotify,
+  useCreate,
+  useRefresh,
 } from "react-admin";
 import { formatMoney } from "../../utils";
+import { SwapHoriz } from "@mui/icons-material";
+
+const JournalShowActions = () => {
+  const record = useRecordContext();
+
+  const notify = useNotify();
+  const [toggle, { isPending }] = useCreate();
+  const refresh = useRefresh();
+
+  const handleClick = () => {
+    toggle(
+      "rpc/toggle_journal_sides",
+      { data: { id_param: record?.id } },
+      {
+        onSuccess: () => {
+          notify("Partida invertida");
+          refresh();
+        },
+        onError: () => notify("Error", { type: "error" }),
+      },
+    );
+  };
+
+  return (
+    <TopToolbar>
+      <Button
+        disabled={isPending}
+        color="primary"
+        startIcon={<SwapHoriz />}
+        label="Invertir partida"
+        onClick={() => handleClick()}
+      />
+      <EditButton />
+    </TopToolbar>
+  );
+};
 
 export const JournalShow = () => (
-  <Show queryOptions={{ meta: { embed: ["journal_entries(*, accounts(*))"] } }}>
+  <Show
+    actions={<JournalShowActions />}
+    queryOptions={{ meta: { embed: ["journal_entries(*, accounts(*))"] } }}
+  >
     <SimpleShowLayout>
       <TextField source="journal_number" />
       <ReferenceField source="transaction_id" reference="transactions" />
@@ -34,7 +80,7 @@ export const JournalShow = () => (
             link={false}
           >
             <TextField source="description" record={record} />
-            <br/>
+            <br />
             <TextField source="name" record={record.accounts} />
           </ReferenceField>
         )}
